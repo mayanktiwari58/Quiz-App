466430237c382854c88bd6c477daf29c8f60659c
mayanktiwari58 <mt3260273@gmail.com>
2024-03-14 08:55:46 +0530

    Add component of Money List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import moneyList from "./component/moneyList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">main</div>
+      <div className="pyramid">
+        <ul className="moneyList">
+        {moneyList.map((m)=>(
+          <li className="moneyListItem ">
+            <span className="moneyListItemNumber">{m.id}</span>
+            <span className="moneyListItemAmount">{m.amount}</span>
+          </li>
+        ))}
+          
+        </ul>
+      </div>
     </div>
   );
 }
