bb06002c562fab260d02a398c3cbcf9d79f284ca
mayanktiwari58 <mt3260273@gmail.com>
2024-04-15 09:09:19 +0530

    Add component of Money List

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 import moneyList from "./component/moneyList";
+import QuestionList from "./component/questionList";
 
 function App() {
+  const [questionNumber, setQuestionNumber] = useState(1);
+
   return (
     <div className="App">
-      <div className="main">main</div>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">
+        <QuestionList/>
+        </div>
+      </div>
       <div className="pyramid">
         <ul className="moneyList">
-        {moneyList.map((m)=>(
-          <li className="moneyListItem ">
-            <span className="moneyListItemNumber">{m.id}</span>
-            <span className="moneyListItemAmount">{m.amount}</span>
-          </li>
-        ))}
-          
+          {moneyList.map((m) => (
+            <li
+              className={
+                questionNumber === m.id
+                  ? "moneyListItem active"
+                  : "moneyListItem"
+              }
+            >
+              <span className="moneyListItemNumber">{m.id}</span>
+              <span className="moneyListItemAmount">{m.amount}</span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
---
+++ src/component/questionList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const QuestionList = () => {
+  return (
+    <div className='trivia'>
+      <div className='question'>What is the best youtube channel</div>
+      <div className='answers'>
+        <div className='answer'>Technical</div>
+        <div className='answer'>Technical</div>
+        <div className='answer'>Technical</div>
+        <div className='answer'>Technical</div>
+      </div>
+    </div>
+  );
+}
+
+export default QuestionList;
