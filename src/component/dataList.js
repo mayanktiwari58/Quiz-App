83771da85e3d91d760dab803f4e6c97c3ee204c5
mayanktiwari58 <mt3260273@gmail.com>
2024-04-15 10:31:46 +0530

    Add component of dataList

---
+++ src/component/dataList.js
@@ -0,0 +1,75 @@
+const dataList = [
+  {
+    id: 1,
+    question:
+      "Total bits used by the IPv6 address is?",
+    answer: [
+      {
+        text: "64 bit",
+        correct: false,
+      },
+      {
+    text: "256 bit",
+        correct: false,
+      },
+      {
+        text: "128 bit",
+        correct: true,
+      },
+      {
+        text: "32 bit",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    question:
+      "What is the process at the most detailed level of the data flow diagrams known as?",
+    answer: [
+      {
+        text: "functional primitives",
+        correct: true,
+      },
+      {
+        text: "Interface",
+        correct: false,
+      },
+      {
+        text: "Data Flow",
+        correct: false,
+      },
+      {
+        text: "Transform description",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    question:
+      "Identify the language which is mainly used for Artificial Intelligence?",
+    answer: [
+      {
+        text: "Java",
+        correct: false,
+      },
+      {
+        text: "Prolog",
+        correct: true,
+      },
+      {
+        text: "J2EE",
+        correct: false,
+      },
+      {
+        text: "C",
+        correct: false,
+      },
+    ],
+  },
+];
+
+export default dataList;
