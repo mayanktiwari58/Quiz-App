d7b6badb86e6f811c7f125dfd15efae26277ba1d
mayanktiwari58 <mt3260273@gmail.com>
2024-04-15 09:15:12 +0530

    Add component of QuestionList

--- src/component/questionList.js
+++ src/component/questionList.js
@@ -5,7 +5,7 @@ const QuestionList = () => {
     <div className='trivia'>
       <div className='question'>What is the best youtube channel</div>
       <div className='answers'>
-        <div className='answer'>Technical</div>
+        <div className='answer '>Technical</div>
         <div className='answer'>Technical</div>
         <div className='answer'>Technical</div>
         <div className='answer'>Technical</div>
