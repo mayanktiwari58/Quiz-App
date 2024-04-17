06d7bbb8bd5950e572c6c966e5adc0cea2011a94
mayanktiwari58 <mt3260273@gmail.com>
2024-04-17 17:04:04 +0530

    Display question and answer with option

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { useState } from "react";
 import "./App.css";
 import moneyList from "./component/moneyList";
 import QuestionList from "./component/questionList";
-
+import dataList from "./component/dataList";
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
+  const [timeOut, setTimeOut] = useState(false);
 
   return (
     <div className="App">
@@ -13,7 +14,12 @@ function App() {
           <div className="timer">30</div>
         </div>
         <div className="bottom">
-        <QuestionList/>
+          <QuestionList
+            dataList={dataList}
+            setTimeOut={setTimeOut}
+            questionNumber={questionNumber}
+            setQuestionNumber={setQuestionNumber}
+          />
         </div>
       </div>
       <div className="pyramid">
--- src/component/questionList.js
+++ src/component/questionList.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-const QuestionList = () => {
+const QuestionList = ({
+  dataList,
+  setTimeOut,
+  questionNumber,
+  setQuestionNumber,
+}) => {
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+
+  useEffect(() => {
+    setQuestion(dataList[questionNumber - 1]);
+  }, [dataList, questionNumber]);
+
+  const handleClick = (a) => {
+    setSelectedAnswer(a);
+    setClassName("answer active");
+    setTimeOut(()=>{
+      setClassName(a.correct ? "answer correct" : "answer wrong")
+    },3000)
+  };
+
   return (
-    <div className='trivia'>
-      <div className='question'>What is the best youtube channel</div>
-      <div className='answers'>
-        <div className='answer '>Technical</div>
-        <div className='answer'>Technical</div>
-        <div className='answer'>Technical</div>
-        <div className='answer'>Technical</div>
+    <div className="trivia">
+      <div className="question">{question?.question}</div>
+      <div className="answers">
+        {question?.answer.map((a) => (
+          <div
+            className={selectedAnswer === a ? className : "answer"}
+            onClick={() => handleClick(a)}
+          >
+            {a.text}
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default QuestionList;
